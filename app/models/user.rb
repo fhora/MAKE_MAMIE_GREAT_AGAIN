7a95730d99b3107f3fbf6d2ce7f9a6da511a3132
Gautier David <gautier.d.91@gmail.com>
2022-11-28 15:16:20 +0100

    add association to user and mission models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :mission_candidates
+  has_many :missions, through: :mission_candidates
 end
