37a747793a1a85152498390e0d5b3a552c9cb6a6
eliott norie <eliott.norie@gmail.com>
2022-11-28 13:28:59 +0100

    perfect missions

--- db/migrate/20221128135549_changing_mission_table.rb
+++ db/migrate/20221128135549_changing_mission_table.rb
@@ -2,6 +2,9 @@ class ChangingMissionTable < ActiveRecord::Migration[7.0]
   def change
     remove_column :missions, :start_date, :datetime
     remove_column :missions, :end_date, :datetime
+  end
+
+  def add
     add_column :missions, :start_date, :datetime
     add_column :missions, :end_date, :datetime
   end
