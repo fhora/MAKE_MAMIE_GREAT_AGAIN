ab27b46461ebd93ab4a2cb697a97a1c9bbadbdbe
fabiola hora <fabiola.hordinola@gmail.com>
2022-11-28 17:10:26 +0100

    controller mission_candidate

---
+++ app/controllers/mission_candidate_controller.rb
@@ -0,0 +1,16 @@
+class MissionCandidateController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'mission_candidate/new'
+  get 'mission_candidate/create'
+  get 'mission_candidate/update'
+  get 'mission_candidate/edit'
+  get 'mission_candidate/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
