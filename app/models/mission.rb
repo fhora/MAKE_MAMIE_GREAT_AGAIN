4985f50376ff4ba9fa17462f1006086b1185eb37
eliott norie <eliott.norie@gmail.com>
2022-11-29 10:25:58 +0100

    added index show

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,7 +1,5 @@
 class Mission < ApplicationRecord
   belongs_to :user
-  has_many :mission_candidates
-  has_many :users, through: :mission_candidates
 
   validates :title, presence: true, length: { maximum: 20 }
   validates :description, presence: true, length: { minimum: 10 }
