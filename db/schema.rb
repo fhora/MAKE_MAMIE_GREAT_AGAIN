8cf18f61d9d0a16e8d1c7d611991311a9f33f18c
Gautier David <gautier.d.91@gmail.com>
2022-11-30 12:13:09 +0100

    add review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_105445) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_110606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_105445) do
     t.index ["user_id"], name: "index_missions_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,4 +70,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_105445) do
   add_foreign_key "mission_candidates", "missions"
   add_foreign_key "mission_candidates", "users"
   add_foreign_key "missions", "users"
+  add_foreign_key "reviews", "users"
 end
