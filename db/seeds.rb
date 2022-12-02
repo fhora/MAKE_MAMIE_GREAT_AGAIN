60908648985d62213b03b1935712ca2a1526bfe4
Aaron Delabays <aaron@delabaysconseil.com>
2022-12-02 13:48:38 +0100

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 User.destroy_all
+ADDRESS = ["16 villa gaudelet, Paris", "63 avenue parmentier, Paris", "Pigalle, Paris", "Eiffel Tower, Paris", "Montparnasse Cemetery, Paris", "23 Rue Blomet, Paris"]
+START_DATE = ["Sun, 01 Jan 2023 14:00:00.000000000 UTC +00:00", "Mon, 02 Jan 2023 10:00:00.000000000 UTC +00:00", "Tue, 29 Nov 2022 15:30:00.000000000 UTC +00:00", "Wed, 23 Nov 2022 17:30:00.000000000 UTC +00:00"]
+
 arr = ["a","b","c"]
 arr.each do |letter|
  user = User.new(email: "#{letter}@#{letter}.com", first_name: "#{letter}", last_name: "#{letter}", password: "testtest")
@@ -9,19 +12,22 @@ end
 
 require "faker"
 categories = ["Grocery shopping", "Walking", "Medical appointment", "Reading", "Playing cards", "Discussing", "Tea time"]
-50.times do
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
   user = User.new(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+    first_name: first_name,
+    last_name: last_name,
     password: "testtest",
-    email: "#{Faker::Name.first_name}.#{Faker::Name.last_name}@gmail.com"
+    email: "#{first_name}.#{last_name}@gmail.com"
   )
   Mission.create!(
-    user:,
+    user: user,
     title: "#{categories.sample} with #{user.first_name}",
     description: "#{user.first_name} is very kind and funny, you'll enjoy spending time together!",
     reward_cents: rand(10_000),
-    location: "#{Faker::Address.street_address}, #{Faker::Address.city}"
+    start_date: START_DATE.sample,
+    location: ADDRESS.sample
   )
   user.save
 end
