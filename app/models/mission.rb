f738567047561443357909e443c6d7e2fd88ee07
Aaron Delabays <aaron@delabaysconseil.com>
2022-12-02 11:28:22 +0100

    missions showv1

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,6 +2,8 @@ class Mission < ApplicationRecord
   belongs_to :user
   has_many :mission_candidates, dependent: :destroy
 
+  has_rich_text :content
+
   validates :title, presence: true, length: { maximum: 20 }
   validates :description, presence: true, length: { minimum: 10 }
   validates :reward_cents, presence: true, numericality: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@ arr.each do |letter|
   Mission.create(user: user, title: "#{user.first_name}'s mission", description: "#{user.first_name}'s mission description", reward_cents: 10000, location: "24 rue jean baptiste pigalle")
   user.save
 end
+
+user1 = User.new(email: "paul@gmail.com", first_name: "Paul", last_name: "Denis-Farge", password: "testest", phone_number: "0678901665", description:  "Je m'appelle Paul, j'ai 78ans.\r\n\r\nJe vis seul en autonomie à Paris mais je cherche régulièrement une personne pour m'accompagner faire une promenade le long du canal Saint-Martin.\r\n\r\nJe préfère les profils discrets et bienveillants.\r\n\r\nJe suis également capable d'échanger en anglais.")
+user1.save
+mission = Mission.create!(user: user1, title: "Bring me to the walk", description: "I would like to be with some company to walk through the park in Paris on Sunday", start_date: DateTime.parse("01/01/2023 14:00"), location: "48, rue du Chateau Landon, 75010 Paris", reward_cents: 10000)
